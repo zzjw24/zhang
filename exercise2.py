ea379db1e108347327fae3b1f3a3b0432545c220
zzjw24 <113301802+zzjw24@users.noreply.github.com>
2022-10-07 19:00:12 +0800

    Add files via upload
    学号：2020144138
    姓名：张志娟
    书上练习2

--- exercise2.py
+++ exercise2.py
@@ -1,7 +1,25 @@
-str='X-DSPAM-Confidence: 0.8475'
-sppos=str.find(' ')
-print(sppos)
-num=str[sppos+1:]
-print(num)
-print(type(num))
-print(type(float(num)))
+from threading import Thread
+from tkinter import *
+import time
+import tkinter
+
+def fun():
+    localtime = time.asctime( time.localtime(time.time()) )
+    print(localtime)
+
+def fun1():
+    txt=entry.get()
+    time = time.asctime(time.localtime(time.time()))
+    entry.__hash__(time)
+window =tkinter.Tk()
+window.title('show time')
+window.geometry('1000x200')
+
+button=tkinter.Button(window,text='显示时间',bg='#CC33CC', command=lambda : fun())
+button.pack()
+
+#time = time.asctime( time.localtime(time.time()) )
+entry=tkinter.Entry(window,text='',bg='white',bd=20,selectbackground='red',show='txt')
+entry.pack()
+
+top=mainloop()
